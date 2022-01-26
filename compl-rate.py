f0999bfb88b785a684d3b69ac6701083fcd5f1ee
xabk <xabk+github@xabk.net>
2022-01-26 15:28:50 +0400

    Compl rates uses configs, supports several loc targets

--- compl-rate.py
+++ compl-rate.py
@@ -1,13 +1,11 @@
 import csv
-import polib
 import os
-import argparse
-import crowdin
+from dataclasses import dataclass, field
+from pathlib import Path
+from loguru import logger
 
-# TODO: Use loguru for logging
-
-# TODO: Add config file support
-# TODO: Move parameters to config file
+from libraries import utilities, polib
+from libraries.crowdin import UECrowdinClient
 
 # TODO: Add parameter to control fall back to PO file stats (line 73)
 
@@ -16,102 +14,197 @@ import crowdin
 # ----------------------------------------------------------------------------------------------------
 # Parameters - These can be edited
 
-CSV_FILENAME = 'Localization/DT_OptionsMenuLanguages.csv'  # Relative to Content the project directory
-TARGET = 'Game'  # Game localization target
-POFILE = 'Localization/{target}/{locale}/Game.po'  # Relative to Content the project directory
-CSV_ENCODING = 'utf-16-le'
-PO_ENCODING = 'utf-8-sig'
-PO_COMPL_MOD = (
-    100 / 88
-)  # TODO Use this to scale completion (X*MOD) to account for hidden/debug lines
 
-# ----------------------------------------------------------------------------------------------------
+@dataclass
+class CommunityCreditsUpdater(utilities.Parameters):
 
+    # Declare Crowdin parameters to load them from config
+    token: str = None
+    organization: str = None
+    project_id: int = None
 
-def update_completion_rates(
-    filename=CSV_FILENAME,
-    target=TARGET,
-    popath=POFILE,
-    csv_encoding=CSV_ENCODING,
-    po_encoding=PO_ENCODING,
-):
-    """
-    Updates completion rates for all languages listed in the languages CSV
-    by getting translated percetanges from POs it respective locale folders
-    """
+    # TODO: Process all loc targets if none are specified
+    # TODO: Change lambda to empty list to process all loc targets when implemented
+    loc_targets: list = field(
+        default_factory=lambda: ['Game']
+    )  # Localization targets, empty = process all targets
 
-    fields = []
-    rows = []
-    with open(filename, mode='r', encoding=csv_encoding, newline='') as csv_file:
-        csv_reader = csv.reader(csv_file)
-        fields = next(csv_reader)
-        for row in csv_reader:
-            rows.append(row)
+    cultures_to_skip: list = field(
+        default_factory=lambda: ['en-US-POSIX', 'io']
+    )  # Locales to skip (native, debug, etc.)
 
-    processed = 0
+    csv_name: str = 'Localization/DT_OptionsMenuLanguages.csv'  # Relative to Content the project directory
+    po_name: str = 'Localization/{target}/{locale}/{target}.po'  # Relative to Content the project directory
 
-    completion_rates = crowdin.get_completion_rates(crowdin.init_crowdin())
+    csv_encoding: str = 'utf-16-le'
+    po_encoding: str = 'utf-8-sig'
 
-    if len(completion_rates) > 0:
-        print('Got completion rates from Crowdin!')
+    po_completion_threshold: int = 100  # PO-based completion percent to use as 100%
 
-    for row in rows:
-        # TODO: Extract native culture to parameters
-        # Skip the native and test cultures (100% anyway)
-        if row[0] in ['en-US-POSIX', 'io']:
-            continue
+    # TODO: Do I need this here? Or rather in smth from uetools lib?
+    content_dir: str = '../'
 
-        if len(completion_rates) > 0:
-            if row[0] not in completion_rates:
-                print(row[0] + 'missing from language mappings')
-            print(
-                row[0]
-                + ' updated from '
-                + row[4]
-                + ' to '
-                + str(completion_rates[row[0]]['translationProgress'])
-            )
-            row[4] = completion_rates[row[0]]['translationProgress']
+    _csv_path: Path = None
+    _content_path: Path = None
+
+    def post_update(self):
+        super().post_update()
+        self._content_path = Path(self.content_dir)
+        self._csv_path = self._content_path / self.csv_name
+
+    def update_completion_rates_for_target(self, target: str):
+        """
+        Updates completion rates for all languages listed in the languages CSV
+        by getting translated percetanges from POs it respective locale folders
+        """
+
+        fields = []
+        rows = []
+        with open(
+            self._csv_path, mode='r', encoding=self.csv_encoding, newline=''
+        ) as csv_file:
+            csv_reader = csv.reader(csv_file)
+            fields = next(csv_reader)
+            for row in csv_reader:
+                rows.append(row)
+
+        locales_processed = 0
+        locales_skipped = 0
+
+        crowdin = UECrowdinClient(
+            self.token, logger, self.organization, self.project_id
+        )
+
+        completion_rates = crowdin.get_completion_rates(filename=target + '.po')
+
+        if completion_rates:
+            logger.info(f'Got completion rates for {target}.po from Crowdin:')
+            logger.info(completion_rates)
         else:
-            # Check if the file exists, open it and get completion rate
-            curr_path = popath.format(target=target, locale=row[0])
-            if os.path.isfile(curr_path):
-                po = polib.pofile(curr_path)
-                if po.percent_translated() > 1 / PO_COMPL_MOD:
-                    print(
-                        curr_path
-                        + ' updated from '
-                        + row[4]
-                        + ' to 100 (rounded up from 95+)'
-                    )
-                    row[4] = '100'
-                else:
-                    print(
-                        curr_path
-                        + ' updated from '
-                        + row[4]
-                        + ' to '
-                        + str(po.percent_translated())
-                    )
-                    row[4] = str(po.percent_translated())
-                processed += 1
-            else:
-                print(curr_path + ' skipped: no PO file found.')
+            logger.warning(
+                f'No completion rates for {target}.po recieved from Crowdin! '
+                f'Using PO file completion rates with multiplier: {self.po_completion_threshold}.'
+            )
 
-    with open(filename, 'w', encoding=csv_encoding, newline='') as csv_file:
-        csv_writer = csv.writer(csv_file)
-        csv_writer.writerow(fields)
-        csv_writer.writerows(rows)
+        for row in rows:
+            # Skip the native and test cultures (100% anyway)
+            if row[0] in self.cultures_to_skip:
+                logger.info(
+                    f'{row[0]} skipped because it\'s in the locales to skip list.'
+                )
+                locales_skipped += 1
+                continue
+
+            if completion_rates and row[0] in completion_rates:
+                logger.info(
+                    f'{row[0]} updated from {row[4]} to {completion_rates[row[0]]["translationProgress"]} '
+                    '(Crowdin data).'
+                )
+                row[4] = completion_rates[row[0]]['translationProgress']
+                locales_processed += 1
+            else:
+                if completion_rates:
+                    logger.warning(
+                        f'{row[0]} missing from language mappings. Updating using the PO file.'
+                    )
+                else:
+                    logger.warning(
+                        f'No completion rates from Crowdin. Updating {row[0]} using the PO file.'
+                    )
+                # Check if the file exists, open it and get completion rate
+                curr_path = Path(self.po_name.format(target=target, locale=row[0]))
+                if curr_path.exists() and curr_path.is_file():
+                    po = polib.pofile(curr_path, encoding=self.po_encoding)
+                    new_completion_rate = 0
+                    if po.percent_translated() >= self.po_completion_threshold:
+                        new_completion_rate = 100
+                    else:
+                        new_completion_rate = round(
+                            self.po_completion_threshold / po.percent_translated()
+                        )
+
+                    logger.info(
+                        f'{curr_path} updated from {row[4]} to {str(po.percent_translated())} '
+                        '(PO-based completion rate).'
+                    )
+                    row[4] = new_completion_rate
+                    locales_processed += 1
+                else:
+                    logger.error(
+                        f'{row[0]} skipped: no info from Crowdin and no PO file found at {curr_path}.'
+                    )
+                    locales_skipped += 1
+
+        with open(
+            self._csv_path, 'w', encoding=self.csv_encoding, newline=''
+        ) as csv_file:
+            csv_writer = csv.writer(csv_file)
+            csv_writer.writerow(fields)
+            csv_writer.writerows(rows)
+
+        if locales_processed:
+            logger.info(
+                f'Processed locales: {locales_processed} / {len(rows)}. Locales skipped: {locales_skipped}.'
+            )
+            return True
+        else:
+            logger.warning(f'No locales processed for target: {target}.')
+
+        return False
+
+    def update_completion_rates(self):
+        logger.info(
+            f'Targets to process ({len(self.loc_targets)}): {self.loc_targets}.'
+        )
+
+        targets_processed = []
+        for t in self.loc_targets:
+            if self.update_completion_rates_for_target(t):
+                targets_processed += [t]
+
+        if targets_processed:
+            logger.info(
+                f'Targets processed ({len(targets_processed)} / {len(self.loc_targets)}): {targets_processed}.'
+            )
+            return True
+
+        logger.warning('No targets processed.')
+
+        return False
 
 
 def main():
 
-    filename = '../' + CSV_FILENAME  # Assume we're in /Game/Content/Python directory
-    popath = '../' + POFILE  # Assume we're in /Game/Content/Python directory
+    logger.add(
+        'logs/locsync.log',
+        rotation='10MB',
+        retention='1 month',
+        enqueue=True,
+        format='{time:YYYY-MM-DD at HH:mm:ss} | {level} | {message}',
+        level='INFO',
+        encoding='utf-8',
+    )
 
-    update_completion_rates(filename=filename, popath=popath)
+    logger.info('')
+    logger.info(
+        '--- Pull completion rates from Crowdin and update language list CSV ---'
+    )
+    logger.info('')
 
-    return 0
+    cfg = CommunityCreditsUpdater()
+
+    cfg.read_config(Path(__file__).name, logger)
+
+    result = cfg.update_completion_rates()
+
+    logger.info('')
+    logger.info('--- Completion rates script end ---')
+    logger.info('')
+
+    if result:
+        return 0
+
+    return 1
 
 
 # Run the main functionality of the script if it's not imported
