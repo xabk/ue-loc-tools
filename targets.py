56d2020b167f47b51f8077b5ff076061ad5ce850
xabk / Alexander Bukhonov <47788482+xabk@users.noreply.github.com>
2022-08-25 15:03:14 +0200

    added cmd line argument parsing

--- targets.py
+++ targets.py
@@ -1,169 +1,181 @@
+import argparse
 from pathlib import Path
+from typing import NamedTuple
 from loguru import logger
-import re
 
 from dataclasses import dataclass, field
 
 from libraries.utilities import LocTask
 from libraries.uetools import UELocTarget
 
+LOC_TARGET_ACTIONS = ['add', 'replace', 'delete']
+
+
+class LocTargetParameters(NamedTuple):
+    action: str
+    source: str
+    targets: list[str]
+    locales: list[str]
+
 
 @dataclass
-class AddLocales(LocTask):
+class LocaleTask(LocTask):
+    '''
+    Class to represent loc target related tasks
 
-    # TODO: Process all loc targets if none are specified
-    # TODO: Change lambda to None to process all loc targets when implemented
-    loc_targets: list = field(
-        default_factory=lambda: ['Game']
-    )  # Localization targets, empty = process all targets
+    ...
+
+    Attributes
+    ----------
+    source_target
+        Target to use as source (e.g., to add/copy locales from)
+    loc_targets
+        List of targets or a target to modify (add/remove/replace locales)
+        Defaults to None to prevent unintentional modifications
+    project_dir
+        Project directory
+        Defaults to ../../ based on the scripts being in Content/Python
+    '''
+
+    source_target: str = None
+
+    loc_targets: list[str] or str = None
+
+    locales: list[str] = None
 
     # TODO: Do I need this here? Or rather in smth from uetools lib?
-    content_dir: str = '../'
-    config_dir: str = '../../'  # Will try to find it if None or empty
-    # TODO: Use uetools to find the directories?
+    project_dir: str = '../../'
 
-    _content_path: Path = None
-    _config_path: Path = None
+    _source_target: UELocTarget = None
+    _loc_targets: list[UELocTarget] = None
 
-    def post_update(self):
+    _project_path: Path = None
+
+    def post_update(self) -> bool:
         super().post_update()
-
-        self._content_path = Path(self.content_dir).resolve()
 
         if self.project_dir:
             self._project_path = Path(self.project_dir).resolve()
         else:
-            self._project_path = self._content_path.parent.resolve()
+            self._project_path = self._project_path.parent.resolve()
 
-        try:
-            self._uproject_path = next(self._project_path.glob('*.uproject'))
-        except Exception as err:
+        if not (
+            (self._project_path / 'Content').exists()
+            and (self._project_path / 'Config').exists()
+        ):
             logger.error(
-                f'Seems like no .uproject file found in {self._project_path}. '
-                'Wrong path?'
+                f'{self._project_path} doesn\'t '
+                'look like an Unreal project directory'
             )
-            logger.error(err)
-            return False
-
-        if self.engine_dir:
-            self._engine_path = Path(self.engine_dir).resolve()
-        else:
-            # Try to find it as if we're in Games/..
-            logger.info('Checking if engine path is ../../ from project directory.')
-            self._engine_path = self._project_path.parent.parent
-            self._unreal_binary_path = self._engine_path / self._unreal_binary
-
-            if not self._unreal_binary_path.exists():
-                # Try to find it in the .sln file
-                solution_file = next(self._project_path.glob('*.sln'))
-                logger.info(
-                    f'Trying to find the engine path from solution file: '
-                    f'{solution_file}'
-                )
-                if not solution_file.exists():
-                    logger.error(
-                        f'No solution file found in {self._project_path}. Aborting. '
-                        'Try setting engine directory explicitely in config.'
-                    )
-                    return False
-
-                with open(solution_file, mode='r') as file:
-                    s = file.read()
-                    engine_path = re.findall(
-                        r'"UnrealBuildTool", "(.*?)Engine\\Source\\Programs'
-                        r'\\UnrealBuildTool\\UnrealBuildTool.csproj"',
-                        s,
-                    )
-
-                if len(engine_path) == 0:
-                    logger.error(
-                        f'Couldn\'t find Engine path in the project solution file: '
-                        '{solution_file}. Aborting. '
-                        'Try setting engine directory explicitely in config.'
-                    )
-                    return False
-
-                # TODO: .sln path absolute if game and engine on different disks?..
-                self._engine_path = (self._project_path / engine_path[0]).resolve()
-                self._unreal_binary_path = self._engine_path / self._unreal_binary
-
-        if not (self._unreal_binary_path and self._unreal_binary_path.exists()):
-            logger.error(
-                f'No unreal binary found for engine path {self._engine_path}. '
-                'Wrong path?'
-            )
-            return False
-
-        self._config_str = ';'.join(
-            [
-                ';'.join(
-                    [
-                        self._config_pattern.format(loc_target=loc_target, task=t)
-                        for t in self.tasks
-                    ]
-                )
-                for loc_target in self.loc_targets
-            ]
-        )
 
         logger.info(f'Project path: {self._project_path}.')
-        logger.info(f'Engine path: {self._engine_path}.')
+
+        if self.source_target is not None:
+            self._source_target = UELocTarget(self._project_path, self.source_target)
+            logger.info(f'Source target: {self.source_target}.')
+        else:
+            logger.info(f'No source target specified.')
+
+        if type(self.loc_targets) is str:
+            self.loc_targets = [self.loc_targets]
+
+        if type(self.loc_targets) is list and len(self.loc_targets) > 0:
+            self._loc_targets = [
+                UELocTarget(self._project_path, target) for target in self.loc_targets
+            ]
+            logger.info(f'Loc targets to modify: {self.loc_targets}.')
+        else:
+            logger.error(f'No loc targets to modify specified.')
 
         return True
 
-    def patch_dependencies(self, loc_target: str):
-        # Patching the gather.ini to fix paths to engine manifest dependencies
-        logger.info('Trying to patch manifest dependencies...')
-        with open(
-            self._project_path / f'Config/Localization/{loc_target}_Gather.ini', 'r'
-        ) as file:
-            gather_ini = file.read()
-            engine_path = re.subn(r'\\', '/', str(self._engine_path))[0]
-            gather_ini, patched_dependencies = re.subn(
-                r'(?<=ManifestDependencies=)[^\r\n]*?(?=Engine/Content/Localization/)',
-                engine_path,
-                gather_ini,
+    def run(self):
+        pass
+
+
+@dataclass
+class ReplaceLocales(LocaleTask):
+    pass
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(
+        description='''
+    Run loc target-related actions. Reads config from base.config.yaml and command line.
+    Examples: targets.py replace source=Game target=Audio
+              targets.py add source=Game target=Audio
+              targets.py delete target=Audio locale=io
+              targets.py add target=Audio locale=io'''
+    )
+
+    parser.add_argument(
+        'action',
+        nargs='?',
+        choices=LOC_TARGET_ACTIONS,
+        help='''Action to perform: `replace`, `add`, or `delete`
+        Replace and add require either -source and -targets or -targets and -locales
+        Delete accepts -targets and -locales''',
+    )
+
+    parser.add_argument(
+        '-source',
+        default=None,
+        nargs=1,
+        help='Source target to use for `replace` and `add` tasks. Defaults to `Game`',
+    )
+
+    parser.add_argument(
+        '-targets',
+        default=None,
+        nargs='*',
+        help='Loc targets to modify, a space-separated list of target names',
+    )
+
+    parser.add_argument(
+        '-locales',
+        default=None,
+        nargs='*',
+        help='Locales to add/replace/delete, a space-separated list of locale names',
+    )
+
+    args, unknown = parser.parse_known_args()
+
+    print(args)
+
+    if args.action is None or args.action not in LOC_TARGET_ACTIONS:
+        logger.error('Argument error: action not supported')
+        return None
+
+    if args.targets is None or type(args.targets) is not list:
+        print(type(args.targets))
+        logger.error('Argument error: targets not specified')
+        return None
+
+    if args.action == 'delete':
+        if args.locales is None or type(args.locales) is not list:
+            logger.error('Argument error: no locales or error for delete action')
+            return None
+
+    if args.source is None:
+        if args.locales is None or type(args.locales) is not list:
+            logger.error(
+                'Argument error: no locales and no source for add or replace action'
             )
+            return None
 
-        if patched_dependencies > 0:
-            with open(
-                self._project_path / f'Config/Localization/{loc_target}_Gather.ini', 'w'
-            ) as file:
-                file.write(gather_ini)
-            logger.info(f'Patched dependencies: {patched_dependencies}')
-        else:
-            logger.info('No dependencies patched.')
-
-        return
-
-    def run_tasks(self):
-        logger.info(
-            f'Processing targets ({len(self.loc_targets)}): '
-            f'{self.loc_targets}. Tasks ({len(self.tasks)}): {self.tasks}'
+    if args.locales is not None and args.source is not None and args.action != 'delete':
+        logger.error(
+            'Argument error: both source and locales specified, specify only one'
         )
+        return None
 
-        if 'Gather' in self.tasks and self.try_patch_dependencies:
-            for loc_target in self.loc_targets:
-                self.patch_dependencies(loc_target)
+    parameters = LocTargetParameters(
+        args.action, args.source, args.targets, args.locales
+    )
 
-        logger.info(
-            f'Running Unreal loc gather commandlet with following config value: '
-            f'{self._config_str}'
-        )
+    logger.info(parameters)
 
-        returncode = 0
-
-        return returncode
-
-
-@dataclass
-class ReplaceLocales(LocTask):
-    pass
-
-
-@dataclass
-class RenameLocale(LocTask):
-    pass
+    return parameters
 
 
 def main():
@@ -181,21 +193,10 @@ def main():
     logger.info('--- Unreal gather text commandlet script ---')
     logger.info('')
 
-    loc_target = UELocTarget(Path('../'), 'Audio')
+    params = parse_arguments()
 
-    # loc_target._rename_loc_folder('en', 'en-US')
-
-    # print(loc_target.get_current_locales())
-    # print(loc_target.get_native_locale())
-
-    # loc_target.replace_all_locales(
-    #     ['en', 'io'],
-    #     keep_native_locale=True,
-    # )
-
-    # task =
-
-    # task.read_config(Path(__file__).name, logger)
+    task = LocaleTask('Game', 'Audio')
+    task.read_config(Path(__file__).name, logger)
 
     returncode = 0  # task.run_tasks()
 
