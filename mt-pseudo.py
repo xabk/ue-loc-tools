eba24b8b2724fe0c746f134a179ef707f52d4ecf
xabk / Alexander Bukhonov <47788482+xabk@users.noreply.github.com>
2023-03-08 15:26:26 +0100

    pre and mt several files

--- mt-pseudo.py
+++ mt-pseudo.py
@@ -28,26 +28,13 @@ class MTPseudo(LocTask):
 
     engine_id: int or None = 1
 
-    file_format: str = 'gettext_unreal'  # gettext_unreal to use the Unreal PO parser on Crowdin
+    file_format: str = (
+        'gettext_unreal'  # gettext_unreal to use the Unreal PO parser on Crowdin
+    )
 
     src_locale: str = 'io'
 
     export_pattern: str = '/{target}/%locale%/{target}.po'
-
-    delete_criteria: list = field(
-        # list of rules, each rule is a list: [property to check, regex, comment to add]
-        #  - property to check: msgid, msgctx, etc. See libraries/polib
-        default_factory=lambda: [
-            [
-                'comment',
-                r'SourceLocation:	/Any/Path/You/Want/',
-            ],
-            [
-                'msgctxt',
-                r'Any_Key_Pattern_You_Want_To_Delete',
-            ],
-        ]
-    )
 
     # TODO: Do I need this here? Or rather in smth from uetools lib?
     content_dir: str = '../'
@@ -58,29 +45,35 @@ class MTPseudo(LocTask):
     _content_path: Path = None
     _temp_path: Path = None
 
+    _crowdin = None
+
     def post_update(self):
         super().post_update()
         self._content_path = Path(self.content_dir).resolve()
         self._fname = self._fname.format(locale=self.src_locale, target='{target}')
         self._temp_path = Path(self.temp_dir).resolve()
-
-    def add_source_files(self) -> dict:
-        crowdin = UECrowdinClient(
+        self._crowdin = UECrowdinClient(
             self.token, logger, self.organization, self.project_id
         )
 
+    def add_source_file(self, fpath: Path) -> int or dict:
+        logger.info(f'Uploading file: {fpath}. Format: {self.file_format}')
+        r = self._crowdin.add_file(
+            fpath,
+            type=self.file_format,
+            export_pattern=self.export_pattern.format(target=target),
+        )
+        return r
+
+    def add_source_files(self) -> dict:
         logger.info(f'Content path: {self._content_path}')
 
         targets_processed = {}
 
         for target in self.loc_targets:
             fpath = self._content_path / self._fname.format(target=target)
-            logger.info(f'Uploading file: {fpath}. Format: {self.file_format}')
-            r = crowdin.add_file(
-                fpath,
-                type=self.file_format,
-                export_pattern=self.export_pattern.format(target=target),
-            )
+            r = self.add_source_file(fpath)
+
             if isinstance(r, int):
                 targets_processed[target] = r
                 logger.info(f'File for {target} added.')
@@ -90,45 +83,69 @@ class MTPseudo(LocTask):
                 )
 
         if len(targets_processed) == len(self.loc_targets):
-            logger.info(f'Targets processed ({len(targets_processed)}): {targets_processed}')
+            logger.info(
+                f'SUCCESS: Targets processed ({len(targets_processed)}): {targets_processed}'
+            )
             return targets_processed
 
         return targets_processed
 
-    def pretranslate(self, file_id: int):
-        crowdin = UECrowdinClient(
-            self.token, logger, self.organization, self.project_id
-        )
-        response = crowdin.translations.apply_pre_translation(
+    def pretranslate_file(self, file_id: int):
+        response = self._crowdin.translations.apply_pre_translation(
             projectId=self.project_id,
             languageIds=self.languages,
             fileIds=[file_id],
         )
 
         if not 'data' in response:
-            logger.error(f'Error. Crowdin response: {response}')
             return response
 
         pre_id = response['data']['identifier']
 
-        response = crowdin.translations.pre_translation_status(self.project_id, pre_id)
-
-        while response['data']['status'] != 'finished':
-            logger.info(f"Progress: {response['data']['progress']}. "
-                        f"ETA: {response['data']['eta']}")
-            sleep(10)
-            response = crowdin.translations.pre_translation_status(self.project_id, pre_id)
-
-        logger.info('SUCCESS: TM pretranslation complete.')
-
-    def mt(self, file_id: int):
-        crowdin = UECrowdinClient(
-            self.token, logger, self.organization, self.project_id
+        response = self._crowdin.translations.pre_translation_status(
+            self.project_id, pre_id
         )
 
+        while response['data']['status'] != 'finished':
+            logger.info(
+                f"Progress: {response['data']['progress']}. "
+                f"ETA: {response['data']['eta']}"
+            )
+            sleep(10)
+            response = self._crowdin.translations.pre_translation_status(
+                self.project_id, pre_id
+            )
+
+        return 0
+
+    def pretranslate_files(self, files: dict[str:int]):
+        logger.info(f'Pretranslating {len(files)} files...')
+        files_processed = {}
+
+        for target, id in files.items():
+            logger.info(f'Pretranslating {target} / {id}')
+            r = self.pretranslate_file(id)
+            if r == 0:
+                files_processed[target] = id
+                logger.info(f'File for {target} pretranslated.')
+            else:
+                logger.error(
+                    f'Something went wrong with {target}. '
+                    f'Here\'s the last response from Crowdin: {r}'
+                )
+
+        if len(files_processed) == len(files):
+            logger.info(
+                f'SUCCESS: Targets pretranslated ({len(files_processed)}): {files_processed}'
+            )
+            return files_processed
+
+        return files_processed
+
+    def mt_file(self, file_id: int):
         # logger.info(crowdin.machine_translations.list_mts())
 
-        response = crowdin.translations.apply_pre_translation(
+        response = self._crowdin.translations.apply_pre_translation(
             projectId=self.project_id,
             languageIds=self.languages,
             fileIds=[file_id],
@@ -137,20 +154,55 @@ class MTPseudo(LocTask):
         )
 
         if not 'data' in response:
-            logger.error(f'Error. Crowdin response: {response}')
             return response
 
         pre_id = response['data']['identifier']
 
-        response = crowdin.translations.pre_translation_status(self.project_id, pre_id)
+        response = self._crowdin.translations.pre_translation_status(
+            self.project_id, pre_id
+        )
 
         while response['data']['status'] != 'finished':
-            logger.info(f"Progress: {response['data']['progress']}. "
-                        f"ETA: {response['data']['eta']}")
+            logger.info(
+                f"Progress: {response['data']['progress']}. "
+                f"ETA: {response['data']['eta']}"
+            )
             sleep(10)
-            response = crowdin.translations.pre_translation_status(self.project_id, pre_id)
+            response = self._crowdin.translations.pre_translation_status(
+                self.project_id, pre_id
+            )
 
-        logger.info('SUCCESS: MT complete.')
+        return 0
+
+    def mt_files(self, files: dict[str:int]):
+        logger.info(f'MT {len(files)} files...')
+        files_processed = {}
+
+        for target, id in files.items():
+            logger.info(f'Pretranslating {target} / {id}')
+            r = self.mt_file(id)
+            if r == 0:
+                files_processed[target] = id
+                logger.info(f'File for {target} machine translated.')
+            else:
+                logger.error(
+                    f'Something went wrong with {target}. '
+                    f'Here\'s the last response from Crowdin: {r}'
+                )
+
+        if len(files_processed) == len(files):
+            logger.info(
+                f'SUCCESS: Targets pretranslated ({len(files_processed)}): {files_processed}'
+            )
+            return files_processed
+
+        return files_processed
+
+    def approve_file(self, file_id: int):
+        pass
+
+    def approve_files(self, files: dict[str:int]):
+        pass
 
     def download_transalted_files(self):
         pass
@@ -163,7 +215,6 @@ class MTPseudo(LocTask):
 
     def add_to_TM(self, file_id: int):
         pass
-
 
 
 def main():
@@ -195,9 +246,13 @@ def main():
 
     # result = task.add_source_files()
 
-    task.pretranslate(948)
+    # task.pretranslate_file(948)
 
-    task.mt(948)
+    # task.mt_file(948)
+
+    task.pretranslate_files({'MTTest': 948})
+
+    task.mt_files({'MTTest': 948})
 
     logger.info('')
     logger.info('--- Add source files on Crowdin script end ---')
