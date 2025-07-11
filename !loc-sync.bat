7c109482ce3ef20f14f46bc49108af05b9d03414
xabk / Alexander Bukhonov <47788482+xabk@users.noreply.github.com>
2025-07-11 09:48:44 +0200

    Switch to uv (docs.astral.sh/uv/)

--- !loc-sync.bat
+++ !loc-sync.bat
@@ -4,23 +4,21 @@
 @echo See logs/locsync.log for logs.
 @echo.
 @echo ------------------------------------------------------------
-@echo Trying to activate the virtual environment...
-@echo Command: call .venv\Scripts\activate.bat
-call .venv\Scripts\activate.bat
+@echo Checking if `uv` is installed...
+uv --version >nul 2>&1
 if %errorlevel% neq 0 (
-  echo Command failed with exit code %errorlevel%.
-  echo Please create a virtual environment under .venv and install the dependencies.
-  pause
+  echo `uv` does not seem to be installed.
+  echo Please install `uv`: https://docs.astral.sh/uv/getting-started/installation/
+  echo Press any key to exit...
+  pause >nul
   exit /b %errorlevel%
 )
-@echo Virtual environment activated.
-@echo.
-@echo.
 
+@echo.
 @echo ------------------------------------------------------------
 @echo @echo Running the loc-sync script in interactive mode...
 @echo Command: python loc-sync.py
-python loc-sync.py
+uv run loc-sync.py %*
 @echo.
 @echo.
 pause
--- #create-venv-for-loc-sync.bat
+++ #create-venv-for-loc-sync.bat
@@ -2,47 +2,16 @@
 @echo Creating venv for loc-sync...
 @echo.
 @echo ------------------------------------------------------------
-@echo Trying to create a virtual environment...
-@echo Replace `projectname` with the prefix you want for the virtual environment
-@echo Command: python -m venv .venv --prompt projectname
-python -m venv .venv --prompt projectname
+@echo Checking if `uv` is installed...
+uv --version >nul 2>&1
 if %errorlevel% neq 0 (
-  echo Command failed with exit code %errorlevel%.
-  echo Please create a virtual environment manually under .venv and install the dependencies.
-  pause
+  echo `uv` does not seem to be installed.
+  echo Please install `uv`: https://docs.astral.sh/uv/getting-started/installation/
+  echo Press any key to exit...
+  pause >nul
   exit /b %errorlevel%
 )
-@echo Virtual environment created.
-@echo.
-@echo.
 
-@echo ------------------------------------------------------------
-@echo Trying to activate the virtual environment...
-@echo Command: call .venv\Scripts\activate.bat
-call .venv\Scripts\activate.bat
-if %errorlevel% neq 0 (
-  echo Command failed with exit code %errorlevel%.
-  echo Please create a virtual environment under manually .venv and install the dependencies.
-  pause
-  exit /b %errorlevel%
-)
-@echo Virtual environment activated.
+@echo `uv` is installed.
+@echo You can ues `uv run loc-sync.py` or the batch files to run the script.
 @echo.
-@echo.
-
-@echo ------------------------------------------------------------
-@echo Trying to install dependencies...
-@echo Command: pip install -r requirements.txt
-pip install -r requirements.txt
-if %errorlevel% neq 0 (
-  echo Command failed with exit code %errorlevel%.
-  echo Please create a virtual environment under manually .venv and install the dependencies.
-  pause
-  exit /b %errorlevel%
-)
-@echo Virtual environment activated.
-@echo.
-@echo.
-@echo on
-
-pause
