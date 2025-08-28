d294bf42d21610c1433498f195c8286743598378
xabk / Alexander Bukhonov <47788482+xabk@users.noreply.github.com>
2025-08-28 11:28:24 +0200

    ~ init for tasks

---
+++ tasks/__init__.py
@@ -0,0 +1,8 @@
+"""
+Tasks module for localization automation.
+
+This module contains all the task classes that can be executed by the TaskRunner.
+Each task should inherit from LocTask and implement the run() method.
+
+Tasks are discovered dynamically based on configuration, not hardcoded imports.
+"""
